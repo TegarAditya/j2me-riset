20d7e3fba11d2562b647bcbd1c56f78238077981
Tegar Aditya <tegaraditya@student.uns.ac.id>
2023-11-23 09:40:51 +0700

    adding github link

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { DarkMode, Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import { GithubSolid } from 'flowbite-svelte-icons';
 	import Logo from '$lib/images/emailkomp.png';
 
 	$: activeUrl = $page.url.pathname;
@@ -11,7 +12,16 @@
 		<img src={Logo} class="mr-3 h-6 sm:h-9" alt="Logo" />
 		<span class="self-center whitespace-nowrap text-xl font-semibold">Divisi Riset</span>
 	</NavBrand>
-	<DarkMode class="ml-auto" />
+	<div class="flex items-center ml-auto">
+		<a
+			class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mx-2"
+			href="https://github.com/TegarAditya/j2me-riset"
+			target="_blank"
+		>
+			<GithubSolid />
+		</a>
+		<DarkMode />
+	</div>
 	<NavHamburger />
 	<NavUl {activeUrl}>
 		<NavLi href="/">Home</NavLi>
